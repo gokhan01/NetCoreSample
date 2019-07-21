e40aeaf834d477bb86f064c16779d114340cdd9d
gokhanuyar <gokhan.uyar@polynomtech.com>
2019-07-21 17:08:09 +0300

    DAL - abstract opr

---
+++ CoreTest.DAL/Abstract/ICategoryDal.cs
@@ -0,0 +1,10 @@
+﻿using CoreTest.Core.DAL;
+using CoreTest.Entities.Concrete;
+
+namespace CoreTest.DAL.Abstract
+{
+    public interface ICategoryDal : IEntityRepository<Category>
+    {
+
+    }
+}
---
+++ CoreTest.DAL/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using CoreTest.Core.DAL;
+using CoreTest.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreTest.DAL.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+
+    }
+}
