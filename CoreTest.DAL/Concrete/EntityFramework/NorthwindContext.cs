da9ef5a4e0a76d74a26fa6f18755306de9da61e6
gokhanuyar <gokhan.uyar@polynomtech.com>
2019-07-21 17:25:54 +0300

    DAL - context

---
+++ CoreTest.DAL/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using CoreTest.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreTest.DAL.Concrete.EntityFramework
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=****; User ID=****; Password=****; Database=Northwind");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
