948f689caf85214db232874515743dd826438280
gokhanuyar <gokhan.uyar@polynomtech.com>
2019-07-21 17:10:50 +0300

    DAL - conrete opr

---
+++ CoreTest.DAL/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,10 @@
+﻿using CoreTest.Core.DAL.EntityFramework;
+using CoreTest.DAL.Abstract;
+using CoreTest.Entities.Concrete;
+
+namespace CoreTest.DAL.Concrete.EntityFramework
+{
+    public class EfCategoryDal : EfEntityRepositoryBase<Category, NorthwindContext>, ICategoryDal
+    {
+    }
+}
---
+++ CoreTest.DAL/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,13 @@
+﻿using CoreTest.Core.DAL.EntityFramework;
+using CoreTest.DAL.Abstract;
+using CoreTest.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreTest.DAL.Concrete.EntityFramework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+    }
+}
