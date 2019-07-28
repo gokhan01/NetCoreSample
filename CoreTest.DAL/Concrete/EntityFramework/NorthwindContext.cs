80a1f19a194fb7cc66ab0ee191d67fc305d68f8f
gokhanuyar <gokhan.uyar@polynomtech.com>
2019-07-28 14:46:19 +0300

    layout, npm ile bootstrap ve jquery

--- CoreTest.DAL/Concrete/EntityFramework/NorthwindContext.cs
+++ CoreTest.DAL/Concrete/EntityFramework/NorthwindContext.cs
@@ -10,7 +10,7 @@ namespace CoreTest.DAL.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=****; User ID=****; Password=****; Database=Northwind");
+            optionsBuilder.UseSqlServer(@"Server=.; User ID=sa; Password=gokhan123; Database=Northwind");
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
